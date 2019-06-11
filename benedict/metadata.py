04af98d5f37ce4a88eb53a6d57f5b6b46fa8d2a2
Fabio Caccamo <fabio.caccamo@gmail.com>
2019-06-11 18:41:01 +0200

    Updated version.

--- benedict/metadata.py
+++ benedict/metadata.py
@@ -2,9 +2,9 @@
 
 __author__ = 'Fabio Caccamo'
 __copyright__ = 'Copyright (c) 2019 Fabio Caccamo'
-__description__ = 'python-benedict is the Python dictionary for humans dealing with evil/complex data.'
+__description__ = 'The Python dictionary for humans dealing with evil/complex data.'
 __email__ = 'fabio.caccamo@gmail.com'
 __license__ = 'MIT'
 __title__ = 'benedict'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
