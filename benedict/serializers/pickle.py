d721d8c98642502dfcd78825a195ec2eef3cc73c
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-02-21 13:53:09 +0100

    Changed pickle default protocol to pickle.HIGHEST_PROTOCOL.

--- benedict/serializers/pickle.py
+++ benedict/serializers/pickle.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 
 from benedict.serializers.abstract import AbstractSerializer
 
+import base64
 import pickle
 
 
@@ -12,10 +13,13 @@ class PickleSerializer(AbstractSerializer):
     def __init__(self):
         super(PickleSerializer, self).__init__()
 
-    def decode(self, b, **kwargs):
-        data = pickle.loads(b, **kwargs)
-        return data
+    def decode(self, s, **kwargs):
+        encoding = kwargs.pop('encoding', 'utf-8')
+        return pickle.loads(
+            base64.b64decode(s.encode(encoding)), **kwargs)
 
     def encode(self, d, **kwargs):
-        data = pickle.dumps(d, **kwargs)
-        return data
+        encoding = kwargs.pop('encoding', 'utf-8')
+        kwargs.setdefault('protocol', pickle.HIGHEST_PROTOCOL)
+        return base64.b64encode(
+            pickle.dumps(d, **kwargs)).decode(encoding)
--- tests/serializers/test_pickle_serializer.py
+++ tests/serializers/test_pickle_serializer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from benedict.serializers import Base64Serializer
 from benedict.serializers import PickleSerializer
+from benedict.utils import type_util
 
 import datetime as dt
 import unittest
@@ -11,17 +11,28 @@ class pickle_serializer_test_case(unittest.TestCase):
 
     # def test_decode_pickle(self):
     #     s = 'gAN9cQBYBAAAAGRhdGVxAWNkYXRldGltZQpkYXRldGltZQpxAkMKB8EEAwAAAAAAAHEDhXEEUnEFcy4='
-    #     d = Base64Serializer().decode(s, subformat='pickle')
+    #     d = PickleSerializer().decode(s)
     #     r = {
     #         'date': dt.datetime(year=1985, month=4, day=3),
     #     }
     #     self.assertEqual(d, r)
 
+    # def test_encode_pickle(self):
+    #     d = {
+    #         'date': dt.datetime(year=1985, month=4, day=3),
+    #     }
+    #     s = PickleSerializer().encode(d)
+    #     r = 'gAN9cQBYBAAAAGRhdGVxAWNkYXRldGltZQpkYXRldGltZQpxAkMKB8EEAwAAAAAAAHEDhXEEUnEFcy4='
+    #     self.assertEqual(s, r)
+
     def test_encode_decode_pickle(self):
         d = {
             'date': dt.datetime(year=1985, month=4, day=3),
         }
-        serializer = Base64Serializer()
-        s = serializer.encode(d, subformat='pickle')
-        r = serializer.decode(s, subformat='pickle')
+        serializer = PickleSerializer()
+        s = serializer.encode(d)
+        print(s)
+        self.assertTrue(type_util.is_string(s))
+        r = serializer.decode(s)
+        self.assertTrue(type_util.is_dict(d))
         self.assertEqual(d, r)
