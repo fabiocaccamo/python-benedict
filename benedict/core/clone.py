7fc6cf78ef427182248a4b8bddd729c43316fd0b
Fabio Caccamo <fabio.caccamo@gmail.com>
2020-12-22 12:50:30 +0100

    Added memo option to clone core method.

--- benedict/core/clone.py
+++ benedict/core/clone.py
@@ -3,8 +3,8 @@
 import copy
 
 
-def clone(obj, empty=False):
-    d = copy.deepcopy(obj)
+def clone(obj, empty=False, memo=None):
+    d = copy.deepcopy(obj, memo)
     if empty:
         d.clear()
     return d
