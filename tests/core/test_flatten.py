75349bf8f1e062072b500131268d8c44b170ad49
Dorian Santner <dorian.santner@providens.at>
2021-08-13 14:16:40 +0200

    fixed tests for issue #63: only flatten() and unflatten() , more tests need adoption for consistently setting the keypath_separator to '.'

--- tests/core/test_flatten.py
+++ tests/core/test_flatten.py
@@ -6,67 +6,66 @@ import unittest
 
 
 class flatten_test_case(unittest.TestCase):
-
     def test_flatten(self):
         i = {
-            'a': 1,
-            'b': 2,
-            'c': {
-                'd': {
-                    'e': 3,
-                    'f': 4,
-                    'g': {
-                        'h': 5,
-                    }
+            "a": 1,
+            "b": 2,
+            "c": {
+                "d": {
+                    "e": 3,
+                    "f": 4,
+                    "g": {
+                        "h": 5,
+                    },
                 }
             },
         }
         o = _flatten(i)
         r = {
-            'a': 1,
-            'b': 2,
-            'c_d_e': 3,
-            'c_d_f': 4,
-            'c_d_g_h': 5,
+            "a": 1,
+            "b": 2,
+            "c.d.e": 3,
+            "c.d.f": 4,
+            "c.d.g.h": 5,
         }
         self.assertEqual(o, r)
 
     def test_flatten_with_custom_separator(self):
         i = {
-            'a': 1,
-            'b': 2,
-            'c': {
-                'd': {
-                    'e': 3,
-                    'f': 4,
-                    'g': {
-                        'h': 5,
-                    }
+            "a": 1,
+            "b": 2,
+            "c": {
+                "d": {
+                    "e": 3,
+                    "f": 4,
+                    "g": {
+                        "h": 5,
+                    },
                 }
             },
         }
-        o = _flatten(i, separator='/')
+        o = _flatten(i, separator="/")
         r = {
-            'a': 1,
-            'b': 2,
-            'c/d/e': 3,
-            'c/d/f': 4,
-            'c/d/g/h': 5,
+            "a": 1,
+            "b": 2,
+            "c/d/e": 3,
+            "c/d/f": 4,
+            "c/d/g/h": 5,
         }
         self.assertEqual(o, r)
 
     def test_flatten_with_key_conflict(self):
         i = {
-            'a': 1,
-            'b': 2,
-            'c': {
-                'd': 3,
+            "a": 1,
+            "b": 2,
+            "c": {
+                "d": 3,
             },
-            'c_d': 4,
-            'd_e': 5,
-            'd': {
-                'e': 6,
-            }
+            "c.d": 4,
+            "d_e": 5,
+            "d": {
+                "e": 6,
+            },
         }
         with self.assertRaises(KeyError):
             o = _flatten(i)
--- tests/core/test_unflatten.py
+++ tests/core/test_unflatten.py
@@ -6,70 +6,69 @@ import unittest
 
 
 class unflatten_test_case(unittest.TestCase):
-
     def test_unflatten(self):
         d = {
-            'a': 1,
-            'b_c': 2,
-            'd_e': 3,
+            "a": 1,
+            "b.c": 2,
+            "d.e": 3,
         }
         u = _unflatten(d)
         r = {
-            'a': 1,
-            'b': {
-                'c': 2,
+            "a": 1,
+            "b": {
+                "c": 2,
             },
-            'd': {
-                'e': 3,
+            "d": {
+                "e": 3,
             },
         }
         self.assertEqual(u, r)
 
     def test_unflatten_with_custom_separator(self):
         d = {
-            'a': 1,
-            'b|c': 2,
-            'd|e': 3,
+            "a": 1,
+            "b|c": 2,
+            "d|e": 3,
         }
-        u = _unflatten(d, separator='#')
+        u = _unflatten(d, separator="#")
         self.assertEqual(u, d)
-        u = _unflatten(d, separator='|')
+        u = _unflatten(d, separator="|")
         r = {
-            'a': 1,
-            'b': {
-                'c': 2,
+            "a": 1,
+            "b": {
+                "c": 2,
             },
-            'd': {
-                'e': 3,
+            "d": {
+                "e": 3,
             },
         }
         self.assertEqual(u, r)
 
     def test_unflatten_with_nested_dict(self):
         d = {
-            'a': 1,
-            'b_c': {
-                'u_v': 2,
+            "a": 1,
+            "b.c": {
+                "u.v": 2,
             },
-            'd_e': {
-                'x_y_z': 3,
+            "d.e": {
+                "x.y.z": 3,
             },
         }
         u = _unflatten(d)
         r = {
-            'a': 1,
-            'b': {
-                'c': {
-                    'u': {
-                        'v': 2,
+            "a": 1,
+            "b": {
+                "c": {
+                    "u": {
+                        "v": 2,
                     },
                 },
             },
-            'd': {
-                'e': {
-                    'x': {
-                        'y': {
-                            'z': 3,
+            "d": {
+                "e": {
+                    "x": {
+                        "y": {
+                            "z": 3,
                         },
                     },
                 },
