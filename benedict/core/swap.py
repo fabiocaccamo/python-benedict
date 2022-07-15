37fed124851e8c5e674ca4aca1320f8c931c7dbd
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-07-15 23:54:34 +0200

    Fixed `swap` between dict items.

--- benedict/core/swap.py
+++ benedict/core/swap.py
@@ -4,4 +4,8 @@
 def swap(d, key1, key2):
     if key1 == key2:
         return
-    d[key1], d[key2] = d[key2], d[key1]
+    val1 = d[key1]
+    val1 = val1.copy() if isinstance(val1, dict) else val1
+    val2 = d[key2]
+    val2 = val2.copy() if isinstance(val2, dict) else val2
+    d[key1], d[key2] = val2, val1
