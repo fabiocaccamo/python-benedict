eb04722d082df2ec1b0ba98ceff36e8101f9bc5c
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-10-05 17:04:23 +0200

    Drop `Python 3.6` support and pin all requirements. (#123)
    * Drop `Python 3.6` official support.
    
    * Pin all requirements.
    
    * Change dependabot target branch.

--- setup.py
+++ setup.py
@@ -122,7 +122,6 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
