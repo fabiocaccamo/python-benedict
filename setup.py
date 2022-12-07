fb512c69696f49dfa73d069954c9b8169e9cc94f
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-12-07 12:06:19 +0100

    Revert "Add `pypi` to CI."
    This reverts commit 77a9d4882bfe506815f4975c1f3ba7869c29496c.

--- setup.py
+++ setup.py
@@ -130,7 +130,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Education :: Testing",
         "Topic :: Software Development :: Build Tools",
         "Topic :: System :: Filesystems",
