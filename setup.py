1c40c4d3fd896dc94eff7948e6f726c1d1049aee
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-12-09 13:37:32 +0100

    Drop `Python 3.7 support`.

--- setup.py
+++ setup.py
@@ -125,7 +125,6 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
