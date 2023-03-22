792348b7b75067a04b0de9ba7113bf7bf91cc5f3
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-03-22 01:21:40 +0100

    Add `keyattr_dynamic` property (`False` by default). #261 (#266)
    * Add `AttributeError` message.
    
    * Add `keyattr_dynamic` property (default `False`).
    
    * Update README.md

--- benedict/dicts/keyattr/keyattr_dict.py
+++ benedict/dicts/keyattr/keyattr_dict.py
@@ -3,9 +3,11 @@ from benedict.dicts.base import BaseDict
 
 class KeyattrDict(BaseDict):
     _keyattr_enabled = None
+    _keyattr_dynamic = None
 
     def __init__(self, *args, **kwargs):
         self._keyattr_enabled = kwargs.pop("keyattr_enabled", True)
+        self._keyattr_dynamic = kwargs.pop("keyattr_dynamic", False)
         super().__init__(*args, **kwargs)
 
     @property
@@ -16,25 +18,34 @@ class KeyattrDict(BaseDict):
     def keyattr_enabled(self, value):
         self._keyattr_enabled = value
 
+    @property
+    def keyattr_dynamic(self):
+        return self._keyattr_dynamic
+
+    @keyattr_dynamic.setter
+    def keyattr_dynamic(self, value):
+        self._keyattr_dynamic = value
+
     def __getattr__(self, attr):
+        attr_message = f"{self.__class__.__name__!r} object has no attribute {attr!r}"
         if not self._keyattr_enabled:
-            raise AttributeError
-        # return super().__getattr__(attr)
+            raise AttributeError(attr_message)
         try:
             return self.__getitem__(attr)
         except KeyError:
             if attr.startswith("_"):
-                raise AttributeError(
-                    f"{self.__class__.__name__!r} object has no attribute {attr!r}"
-                ) from None
+                raise AttributeError(attr_message) from None
+            if not self._keyattr_dynamic:
+                raise AttributeError(attr_message) from None
             self.__setitem__(attr, {})
             return self.__getitem__(attr)
 
     def __setattr__(self, attr, value):
+        attr_message = f"{self.__class__.__name__!r} object has no attribute {attr!r}"
         if attr in self:
             # set existing key
             if not self._keyattr_enabled:
-                raise AttributeError
+                raise AttributeError(attr_message)
             self.__setitem__(attr, value)
         elif hasattr(self.__class__, attr):
             # set existing attr
@@ -42,9 +53,10 @@ class KeyattrDict(BaseDict):
         else:
             # set new key
             if not self._keyattr_enabled:
-                raise AttributeError
+                raise AttributeError(attr_message)
             self.__setitem__(attr, value)
 
     def __setstate__(self, state):
         super().__setstate__(state)
         self._keyattr_enabled = state["_keyattr_enabled"]
+        self._keyattr_dynamic = state["_keyattr_dynamic"]
