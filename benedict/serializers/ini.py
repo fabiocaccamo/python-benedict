84fa1c83a4de7b191a0e797057c0cfa114bb5c18
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-05-16 09:34:13 +0200

    Allow `ini` format to support nested structures. (#289)

--- benedict/serializers/ini.py
+++ benedict/serializers/ini.py
@@ -1,8 +1,10 @@
 from configparser import DEFAULTSECT as default_section
 from configparser import RawConfigParser
 from io import StringIO
+from json.decoder import JSONDecodeError
 
 from benedict.serializers.abstract import AbstractSerializer
+from benedict.serializers.json import JSONSerializer
 from benedict.utils import type_util
 
 
@@ -17,6 +19,7 @@ class INISerializer(AbstractSerializer):
                 "ini",
             ],
         )
+        self._json = JSONSerializer()
 
     @staticmethod
     def _get_parser(options):
@@ -49,9 +52,14 @@ class INISerializer(AbstractSerializer):
         for section in parser.sections():
             data[section] = {}
             for option, _ in parser.items(section):
-                data[section][option] = self._get_section_option_value(
-                    parser, section, option
-                )
+                value = self._get_section_option_value(parser, section, option)
+                if type_util.is_string(value):
+                    try:
+                        value_decoded = self._json.decode(value)
+                        value = value_decoded
+                    except JSONDecodeError:
+                        pass
+                data[section][option] = value
         return data
 
     def encode(self, d, **kwargs):
@@ -63,7 +71,10 @@ class INISerializer(AbstractSerializer):
             section = key
             parser.add_section(section)
             for option_key, option_value in value.items():
-                parser.set(section, option_key, f"{option_value}")
+                if type_util.is_collection(option_value):
+                    parser.set(section, option_key, self._json.encode(option_value))
+                else:
+                    parser.set(section, option_key, option_value)
         str_data = StringIO()
         parser.write(str_data)
         return str_data.getvalue()
