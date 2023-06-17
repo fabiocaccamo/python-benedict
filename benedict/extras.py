0b6f8b508b98e0c2561eba2b3c31398550890292
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-06-17 22:30:03 +0200

    Add `parse` extra (optional dependencies).  (#303)
    * Remove `requests` from requirements.
    
    * Rename internal method.
    
    * Re-add requests requirement.
    
    * Add `parse` extra (optional dependencies).

--- benedict/extras.py
+++ benedict/extras.py
@@ -1,6 +1,7 @@
 from benedict.exceptions import ExtrasRequireModuleNotFoundError
 
 __all__ = [
+    "require_parse",
     "require_s3",
     "require_toml",
     "require_xls",
@@ -9,26 +10,30 @@ __all__ = [
 ]
 
 
-def _require_optional_module(*, target, installed):
+def _require_optional_dependencies(*, target, installed):
     if not installed:
         raise ExtrasRequireModuleNotFoundError(target=target)
 
 
+def require_parse(*, installed):
+    _require_optional_dependencies(target="parse", installed=installed)
+
+
 def require_s3(*, installed):
-    _require_optional_module(target="s3", installed=installed)
+    _require_optional_dependencies(target="s3", installed=installed)
 
 
 def require_toml(*, installed):
-    _require_optional_module(target="toml", installed=installed)
+    _require_optional_dependencies(target="toml", installed=installed)
 
 
 def require_xls(*, installed):
-    _require_optional_module(target="xls", installed=installed)
+    _require_optional_dependencies(target="xls", installed=installed)
 
 
 def require_xml(*, installed):
-    _require_optional_module(target="xml", installed=installed)
+    _require_optional_dependencies(target="xml", installed=installed)
 
 
 def require_yaml(*, installed):
-    _require_optional_module(target="yaml", installed=installed)
+    _require_optional_dependencies(target="yaml", installed=installed)
