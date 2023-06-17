85de7c1f394d18076eccb2002fda6b3269667af9
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-06-17 22:06:37 +0200

    Rename internal method.

--- benedict/extras.py
+++ benedict/extras.py
@@ -9,26 +9,26 @@ __all__ = [
 ]
 
 
-def _require_optional_module(*, target, installed):
+def _require_optional_dependencies(*, target, installed):
     if not installed:
         raise ExtrasRequireModuleNotFoundError(target=target)
 
 
 def require_s3(*, installed):
-    _require_optional_module(target="s3", installed=installed)
+    _require_optional_dependencies(target="s3", installed=installed)
 
 
 def require_toml(*, installed):
-    _require_optional_module(target="toml", installed=installed)
+    _require_optional_dependencies(target="toml", installed=installed)
 
 
 def require_xls(*, installed):
-    _require_optional_module(target="xls", installed=installed)
+    _require_optional_dependencies(target="xls", installed=installed)
 
 
 def require_xml(*, installed):
-    _require_optional_module(target="xml", installed=installed)
+    _require_optional_dependencies(target="xml", installed=installed)
 
 
 def require_yaml(*, installed):
-    _require_optional_module(target="yaml", installed=installed)
+    _require_optional_dependencies(target="yaml", installed=installed)
